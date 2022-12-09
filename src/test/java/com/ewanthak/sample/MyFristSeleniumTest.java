a9346c3118e073994892de88b89037d53be4d8e2
Ewantha Kularathne <ewanthak@superoffice.com>
2022-12-09 14:02:51 +0530

    Adding driver manager dependancy

--- src/test/java/com/ewanthak/sample/MyFristSeleniumTest.java
+++ src/test/java/com/ewanthak/sample/MyFristSeleniumTest.java
@@ -9,16 +9,19 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class MyFristSeleniumTest {
 
 	public WebDriver driver;
-	public String baseUrl = "http://automationpractice.com/index.php";
+	public String baseUrl = "https://www.saucedemo.com/";
 	
 	@BeforeMethod
 	public void setup() {
 		//Setting system properties of ChromeDriver
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe" );
-		//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver.exe");	
+    	WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
@@ -35,7 +38,7 @@ public class MyFristSeleniumTest {
 	
 	@Test
 	public void sampleTest() {
-		Assert.assertEquals(driver.getTitle(), "My Store");
+		Assert.assertEquals(driver.getTitle(), "Swag Labs");
 	}
 	
 	
